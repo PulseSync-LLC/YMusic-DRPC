fafc53fed30a473a5df69370e3b7abdd7c68dc56
EvT <one.evt@gmail.com>
2023-12-05 22:41:34 +0300

    1.0.7 added YDRPC.exe noconsole

--- start.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-if %errorlevel% neq 0 (
-    py .\YandexDiscordRPC\updater.py
-    py .\YandexDiscordRPC\main.py
-) else (
-    python .\YandexDiscordRPC\updater.py
-    python .\YandexDiscordRPC\main.py
-)
