0c5a1d78b5a8dc053547b67053f0063c6c035b82
EvT <one.evt@gmail.com>
2023-12-05 05:16:01 +0300

    testing

--- YandexDiscordRPC/updater.py
+++ YandexDiscordRPC/updater.py
@@ -1,41 +1,45 @@
 import os
 import shutil
-import json
+import configparser
 from git import Repo
 
 repo_url = "https://github.com/Maks1mio/YMusic-DRPC.git"
 local_path = "YMusic-DRPC"
-version_file = os.path.join(local_path, "YandexDiscordRPC", "version.json")
+version_file = os.path.join(local_path, "YandexDiscordRPC", "version.ini")
 
 def update_repository():
     if os.path.exists(local_path):
         repo = Repo(local_path)
         origin = repo.remote(name='origin')
 
+        # Проверьте, есть ли изменения после fetch
+        print("Checking for changes after fetch...")
         origin.fetch()
 
         latest_tag = repo.tags[-1].name
-
         current_version = get_current_version()
 
         if current_version != latest_tag:
-            repo.head.reset(index=True, working_tree=True)
-            origin.pull(latest_tag)
             update_version_file(latest_tag)
+            print("Updating version.ini only...")
+        else:
+            print("No changes in version, skipping update.")
 
 def get_current_version():
+    config = configparser.ConfigParser()
     if os.path.exists(version_file):
-        with open(version_file, 'r') as file:
-            data = json.load(file)
-            print({'current version: ': data})
-            return data.get('version', '')
-    return ""
+        config.read(version_file)
+        current_version = config.get('Version', 'version', fallback='0.0.0')
+        print({'current version: ': current_version})
+        return current_version
+    return "0.0.0"
 
 def update_version_file(version):
-    data = {'version: ': version}
-    with open(version_file, 'w') as file:
-        json.dump(data, file)
-        print(data)
+    config = configparser.ConfigParser()
+    config['Version'] = {'version': version}
+    with open(version_file, 'w') as config_file:
+        config.write(config_file)
+        print(f"Updated version to: {version}")
 
 def copy_files():
     source_dir = os.path.join(local_path, "YandexDiscordRPC")
