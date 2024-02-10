10c5138d420ecdd2741a4767ba7a09aa5398a5f2
EvT <one.evt@gmail.com>
2024-02-11 01:22:40 +0300

    added obs.html overlay

--- YandexDiscordRPC/lib/request_handler.py
+++ YandexDiscordRPC/lib/request_handler.py
@@ -11,6 +11,22 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         self.end_headers()
 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+        if self.path == '/track_info':
+            try:
+                with open('YandexDiscordRPC/data.json', 'r', encoding='utf-8') as json_file:
+                    data = json.load(json_file)
+                    self.wfile.write(json.dumps(data).encode('utf-8'))
+            except Exception as e:
+                self.wfile.write("Error getting track information".encode('utf-8'))
+        else:
+            self.wfile.write("Invalid endpoint".encode('utf-8'))
+
     def do_POST(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
