eb4b7ce1f547af0db6d81c04830a7a3e5697547e
Svyatoslav <zerotwo1337@inbox.ru>
2024-05-28 02:11:51 +0300

    update todo
    Заняло 8 minutes

--- src/renderer/api/interfaces/track.initials.ts
+++ src/renderer/api/interfaces/track.initials.ts
@@ -3,10 +3,10 @@ import TrackInterface from './track.interface'
 const TrackInitials: TrackInterface = {
     playerBarTitle: 'Death Note',
     artist: 'Light',
-    timecodes: ['00:00', '00:10'],
+    timecodes: ['00:00', '14:88'],
     requestImgTrack: [
         null,
-        'https://cdn.discordapp.com/guilds/1227552882744754267/users/449211993585614849/avatars/e522dd8194deca201e48471fddcac6c5.webp?size=2048&format=webp&width=0&height=256',
+        'https://cdn.discordapp.com/avatars/449211993585614849/6c9a8a2b2733b6ea468756e37ab5f7ff.webp?size=2048&format=webp&width=0&height=256',
     ],
     linkTitle: 'worfpjpweiofhoweiufhl',
 }
