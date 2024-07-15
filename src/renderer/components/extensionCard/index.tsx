fbc41deac4b6aa832bc1212b0c10df40fd73094f
EvT <one.evt@gmail.com>
2024-07-16 01:20:20 +0300

    all ok change (add checkbox on extension card)

--- src/renderer/components/extensionCard/index.tsx
+++ src/renderer/components/extensionCard/index.tsx
@@ -29,36 +29,21 @@ const ExtensionCard: React.FC<Props> = ({ theme, isChecked, onCheckboxChange, ch
                     </div>
                 </div>
                 <div className={styles.themeVerStar}>
-                    <div className={styles.tabDetail}><MdDesignServices size={20}/> ver</div>
-                    <div className={styles.star}><MdStar size={20}/> Null</div>
+                    <div className={styles.tabDetail}><MdDesignServices size={20} /> ver</div>
+                    <div className={styles.star}><MdStar size={20} /> Null</div>
                 </div>
             </div>
             <div className={styles.cardContainerTab}>
                 <div className={styles.leftTab}>
-                <div className={styles.tabDetail}><MdFolder size={20}/>--MB</div>
-                </div>
-                <div className={styles.tabDetail}><MdDateRange size={20}/>--d ago (local)</div>
-            </div>
-            {/* <div className={styles.header}>
-                <img
-                    className={styles.icon}
-                    src={theme.image ? theme.path + "\\" + theme.image : "https://cdn.discordapp.com/avatars/449211993585614849/6c9a8a2b2733b6ea468756e37ab5f7ff.png?size=4096"}
-                    width="50"
-                    height="50"
-                    alt="Theme image"
-                />
-                <div className={styles.titleSection}>
-                    <h2 className={styles.title}>{theme.name}</h2>
-                    <span className={styles.author}>{theme.author}</span>
-                </div>
-                <div className={styles.checkbox}>
                     <Checkbox
                         checkType="changeTheme"
                         isChecked={isChecked}
                         onChange={(e: React.ChangeEvent<HTMLInputElement>) => onCheckboxChange(theme.name, e.target.checked)}
                     />
+                    <div className={styles.tabDetail}><MdFolder size={20} />--MB</div>
                 </div>
-            </div> */}
+                <div className={styles.tabDetail}><MdDateRange size={20} />--d ago (local)</div>
+            </div>
         </div>
     )
 }
